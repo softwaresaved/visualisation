82e5dbc3915ca394d27ac8b257bd6c7722f18816
Mike Jackson <michaelj@epcc.ed.ac.uk>
2016-01-07 11:53:51 +0000

    Pulled out project-specific configuration into code_usage (ARCHER) and ssi_projects (SSI)

--- prototypes/codes-force-bubbles/code_usage.php
+++ prototypes/codes-force-bubbles/code_usage.php
@@ -7,6 +7,39 @@
     <link rel="shortcut icon" href="../common/delimited-favicon-v4.ico">
   </head>
   <body>
-  <?php include 'usage.php'; ?>
+
+<script>
+var data_file = 'data/d3codes_summary.csv';
+var category_names = ["Overview",
+		      "Code Type",
+		      "Research Area",
+		      "Programming Language",
+		      "Licence Type"];
+var categories = ["none",
+		  "CodeType",
+		  "ResArea",
+		  "ProgLang",
+		  "LicType"];
+var legend_text = "Number of Users";
+var name_column = "Code";
+var scaling_column = "Usage";
+var level_column = "Users";
+var cull_threshold = 10;
+var scale_bins = [
+		  { label: "Less than 5 users", count: 5, fill: "#fee5d9" },
+		  { label: "5-10 users", count: 10, fill: "#fcbba1" },
+		  { label: "10-20 users", count: 20, fill: "#fc9272" },
+		  { label: "20-50 users", count: 50, fill: "#fb6a4a" },
+		  { label: "50-100 users", count: 100, fill: "#de2d26" },
+		  { label: "More than 100 users", count: 5000, fill: "#a50f15" }
+		  ];
+
+function blob_caption(d) {
+  return "Name: " + d.Code + "<br/>Usage: " + d.Usage + " node hours<br/>Jobs: " + d.Jobs +
+    "<br/>Usage Rank: " + d.Rank + "<br/>Users: " + d.Users;
+}
+</script>
+
+    <?php include 'usage.php'; ?>
   </body>
 </html>
---
+++ prototypes/codes-force-bubbles/ssi_projects.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Software Sustainability Institute Consultancy</title>
+    <link rel="shortcut icon" href="../common/delimited-favicon-v4.ico">
+  </head>
+  <body>
+
+<script>
+var data_file = 'data/SSIConsultancyProjects.csv';
+var category_names = ["Overview",
+		      "Funder",
+		      "Institution",
+		      "Type",
+		      "Research Field"];
+var categories = ["none",
+		  "Funder",
+		  "Institution",
+		  "Type",
+		  "Research Field"];
+var legend_text = "Effort";
+var name_column = "Project Name";
+var scaling_column = "Effort";
+var level_column = "Effort";
+var cull_threshold = 0.25;
+var scale_bins = [
+		  { label: "Less than 0.25 months", count: 0.25, fill: "#fee5d9" },
+		  { label: "0.25-0.5 months", count: 0.5, fill: "#fcbba1" },
+		  { label: "0.5-1 months", count: 1, fill: "#fc9272" },
+		  { label: "1-2 months", count: 2, fill: "#fb6a4a" },
+		  { label: "2-3 months", count: 3, fill: "#de2d26" },
+		  { label: "More than 3 months", count: 5000, fill: "#a50f15" }
+		  ];
+
+function blob_caption(d) {
+  return "Name: " + d["Project Name"] +
+    "<br/>Funder: " + d["Funder"] +
+    "<br/>Institution: " + d["Institution"] +
+    "<br/>Field: " + d["Research Field"] +
+    "<br/>Type: " + d["Type"] +
+    "<br/>Effort: " + d["Effort"] + " staff months";
+}
+</script>
+ 
+   <?php include 'usage.php'; ?>
+  </body>
+</html>
