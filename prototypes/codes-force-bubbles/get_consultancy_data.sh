6461784a1d4b4b60558e372614557cc8749227b3
Mike Jackson <michaelj@epcc.ed.ac.uk>
2016-01-22 12:11:49 +0000

    Updated to reflect change in Python script to transform_consultancy_data.py

--- prototypes/codes-force-bubbles/get_consultancy_data.sh
+++ prototypes/codes-force-bubbles/get_consultancy_data.sh
@@ -5,7 +5,4 @@ CSV_FILE=$2
 
 wget --no-check-certificate --output-document=raw.csv "https://docs.google.com/spreadsheets/d/$DOC_ID/export?gid=0&format=csv"
 
-# Trim first three blank rows from spreadsheet
-awk 'NR > 3 { print }' < raw.csv > trimmed.csv
-
-python filter_ssi_consultancy.py trimmed.csv $CSV_FILE
+python transform_consultancy_data.py raw.csv $CSV_FILE
