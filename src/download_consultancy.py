046f512939eb84034c3b01ea8dc10ed655e392d9
mikej888 <michaelj@epcc.ed.ac.uk>
2016-12-23 09:53:10 +0000

    Removes intermediate files

--- src/download_consultancy.py
+++ src/download_consultancy.py
@@ -4,6 +4,7 @@ number of attendees by institution.
 
 # Copyright (c) 2016 The University of Edinburgh
 
+import os
 import sys
 
 from csv_utils import load_csv_file
@@ -17,16 +18,17 @@ from web_utils import download_google_sheet
 if __name__ == "__main__":
     sheet = sys.argv[1]
     tab = 0
-    file_name = "consultancy_raw.csv"
+    file_name = "tmp.csv"
     download_google_sheet(file_name, sheet, tab)
     # Trim off first three lines
-    trimmed_file_name = "consultancy_raw_trimmed.csv"
+    trimmed_file_name = "tmp_trimmed.csv"
     with open(file_name, 'r') as in_file:
         with open(trimmed_file_name, 'w', newline="") as out_file:
             for _ in range(3):
                 next(in_file)
             for line in in_file:
                 out_file.write(line)
+    os.remove(file_name)
     columns, data = load_csv_file(trimmed_file_name)
     # Split off primary funder into new column.
     columns, data = split_csv_column(
@@ -47,6 +49,7 @@ if __name__ == "__main__":
     save_csv_file(projected_columns,
                   data,
                   "data/sheets/filtered/consultancy.csv")
+    os.remove(trimmed_file_name)
     # Calculate effort per project.
     effort_data = sum_unique_values(data, "Project Name", "PMs")
     save_dict_as_csv_file("data/sheets/filtered/project_effort.csv",
