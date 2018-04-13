6974d6a6e9d14ee9ddbb4935064505eb9b462445
mikej888 <michaelj@epcc.ed.ac.uk>
2018-04-13 11:41:52 +0100

    Added legend to pie.js

--- js/pie.js
+++ js/pie.js
@@ -1,16 +1,15 @@
 /**
  * D3 pie chart
  *
- * This version derived from original downloaded from
- * Pie Chart
+ * Derived from original downloaded from Pie Chart
  * https://bl.ocks.org/mbostock/3887235#index.html
  * on 05/08/2016.
  *
  * Changes: Wrapped code within a function; replaced hard-coded
  * file name, column names, labels with arguments passed in via
- * function call.
+ * function call, draws legend.
  *
- * Uses styles: text, .arc path, .legend rect .legend text
+ * Uses styles: .arc text, .arc path, .wedge-value, .legend text
  *
  * Copyright (c) 2016, Mike Bostock
  * Changes Copyright (c) 2016-2018, The University of Edinburgh.
@@ -27,15 +26,10 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Requires styles for:
- *
- * .arc text - pie chart labels.
- * .arc path - pie chart wedge dividers.
  */
 
 /**
- * Draws pie chart.
+ * Draw pie chart.
  *
  * @param {string} data_file - file with comma-separated values
  * (CSV).
@@ -79,41 +73,82 @@ function draw_pie(data_file,
         .value(function(d) { return d[value_column]; });
 
     var id_tag_link = "#" + id_tag;
-    var svg = d3.select(id_tag_link).append("svg")
+    var element = d3.select(id_tag_link)
+    var svg = element.append("svg")
         .attr("width", width)
-        .attr("height", height)
+        .attr("height", height);
+
+    var svg_g = svg
         .append("g")
-        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+        .attr("transform", "translate(" + width/2 + "," + height/2 + ")");
+
+    var legend_g = svg
+        .append("g")
+        .attr("transform", "translate(" + ((width/2)+radius) + "," + 0 + ")");
+
+    var tooltip = element.append("div")
+        .attr("class", "wedge-value")
+        .style("position", "absolute")
+        .style("visibility", "hidden");
 
     d3.csv(data_file, type, function(error, data) {
         if (error) throw error;
 
-        var g = svg.selectAll(".arc")
+        var g = svg_g.selectAll(".arc")
             .data(pie(data))
             .enter().append("g")
-            .attr("class", "arc");
+            .attr("class", "arc")
+            .on("mouseover", function(d){return tooltip.style("visibility", "visible").text(d.data[label_column] + ": " + d.data[value_column]);})
+            .on("mousemove", function(){return tooltip.style("top", (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");})
+            .on("mouseout", function(){return tooltip.style("visibility", "hidden");})
 
         g.append("path")
             .attr("d", arc)
-            .style("fill", function(d) {
-                var colour = null;
-
-                // d.index corresponds to row index.
-                // data[d.index] == d.data.
-                // d corresponds to row.
-                colour = colours[d.index];
-                return colour;
-              }
+            .style("fill", function(d) { return colours[d.index]; }
         );
 
         g.append("text")
             .attr("transform", function(d) {
-               return "translate(" + labelArc.centroid(d) + ")";
+             return "translate(" + labelArc.centroid(d) + ")";
             })
             .attr("dy", ".35em")
             .text(function(d) {
-                return d.data[label_column] +
-                    "\n(" + d.data[value_column] + ")";
+                if (d.data[value_column] > 0) {
+                    return d.data[label_column] +
+                        "\n(" + d.data[value_column] + ")";
+               } else {
+                   return "";
+               };
             });
+
+        var labels = [];
+        var i = 0;
+        data.forEach(function(d)
+        {
+            labels.push({"label": d[label_column], "colour": colours[i]});
+            i++;
+        });
+
+        var legend_edge = 20;
+        var legend_pad = 10;
+        var legend_height = legend_edge + legend_pad;
+
+        var legend = legend_g.selectAll('.legend')
+            .data(labels)
+            .enter()
+            .append('g')
+            .attr('class', 'legend')
+            .attr('transform', function(d, i) {
+                return 'translate(' + 0 + ',' + (i * legend_height) + ')';
+            });
+        legend.append('rect')
+            .attr('width', legend_edge)
+            .attr('height', legend_edge)
+            .style("fill", function(d) { return d["colour"]; })
+           .style('stroke', colours);
+        legend.append('text')
+            .attr('x', legend_edge + legend_pad)
+            .attr('y', legend_edge - (legend_pad/2))
+            .text(function(d) { return d["label"]; });
     });
 };
